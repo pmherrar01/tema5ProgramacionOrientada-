266f0a4ff78e5c79fb8bab514de243c7e2fc0709
pmherrar01 <pmherrerar01@educarex.es>
2024-01-17 18:12:10 +0100

    Ejercicio 1 por terminar

--- Ejercicio1/src/Coche.java
+++ Ejercicio1/src/Coche.java
@@ -1,4 +1,45 @@
 
 public class Coche {
-
+	private String marca;
+	private String modelo;
+	private String color;
+	private int velocidad;
+	private boolean motorEncendido=false;
+	
+	public void establecerMarca() {
+		
+	}
+	
+	public void establecerModelo() {
+	
+	}
+	
+	public void establecerColor() {
+		
+	}
+	
+	public void arrancarCoche() {
+		motorEncendido=true;
+		velocidad=10;
+		
+	}
+	
+	public void apagarCoche() {
+		motorEncendido=false;
+		velocidad=0;
+	}
+	
+	public void acelerarCoche() {
+		velocidad=velocidad+20;
+	}
+	
+	public void frenarCoche() {
+		velocidad=velocidad-6;
+	}
+	
+	public void obtenerEstado() {
+		System.out.println("El estado del coche " + motorEncendido + velocidad + color + modelo + marca);
+	}
 }
+
+
