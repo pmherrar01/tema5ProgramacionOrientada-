44ec52926ff1a05ca2ff38b325a1cde9b0932571
pmherrar01 <pmherrerar01@educarex.es>
2024-02-01 13:41:34 +0100

    Ejercicio3 repaso por terminar

---
+++ Ejercicio3/src/Rueda.java
@@ -0,0 +1,20 @@
+
+public class Rueda {
+	private String material;
+	private int pulgadas;
+	
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	public int getPulgadas() {
+		return pulgadas;
+	}
+	public void setPulgadas(int pulgadas) {
+		this.pulgadas = pulgadas;
+	}
+	
+	
+}
